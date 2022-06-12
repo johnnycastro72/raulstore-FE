532dc5f4ebbdebcf7cbb821bed0d80ee3bd5e153
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-11 22:11:37 -0500

    Google Authentication
    Google Authentication implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import { stateType } from './state/store'
--- src/components/LeftLogin.tsx
+++ src/components/LeftLogin.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { GoogleAuthProvider, OAuthCredential, signInWithPopup } from 'firebase/auth';
+import { auth } from "../firebaseConfig";
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom'
+import { logInReducer } from '../state/features/loggedInSlice';
 
-const LeftLogin = () => {
+const providerGoogleAuth = new GoogleAuthProvider();
+
+const LeftLogin: React.FC = () => {
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const signInWithGoogleButton = () => {
+
+        signInWithPopup(auth, providerGoogleAuth)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential: OAuthCredential | null = GoogleAuthProvider.credentialFromResult(result);
+
+                const token = credential!.accessToken;
+
+                // The signed-in user info.
+                //If the logged in is succesfull you will acces this part of the code where you will 
+                //get a lot of information about the user that have logged in
+                const user = result.user;
+
+                /*Whit the information of the user you can populate an state that is mainly focused on 
+                  holding the information of the user that is logged in*/
+
+                dispatch(logInReducer(user))
+
+                navigate('/home')
+
+                // ...
+            }).catch((error) => {
+
+                //If the logged in is not succesfull yu will get to this part and with the message you can tell 
+                //the user what went wrong
+
+
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+                navigate('/login')
+            });
+    }
+
     return (
         <div className='loginFrame'>
             <form action="" className='formLogin'>
@@ -10,12 +60,12 @@ const LeftLogin = () => {
                     <h2>Raul's Hardware Store</h2>
                 </div>
                 <h2 className='msg1Login'>Log in to your account</h2>
-                <div id="googleInWrapper">
-                    <div id="customBtn" className="customGPlusSignIn">
-                        <span className="icon"></span>
-                        <span className="buttonText">Google</span>
+                    <div id="googleInWrapper">
+                        <div onClick={signInWithGoogleButton} id="customBtn" className="customGPlusSignIn">
+                            <span onClick={signInWithGoogleButton} className="icon"></span>
+                            <span onClick={signInWithGoogleButton} className="buttonText">Google</span>
+                        </div>
                     </div>
-                </div>
                 <div id="name"></div>
                 <div id="googleInWrapper">
                     <div id="customBtn" className="customGPlusSignIn">
@@ -32,7 +82,7 @@ const LeftLogin = () => {
                         <div className='demailLogin'>
                             <div className='emailFrame'>
                                 <div className='inputFrame'>
-<input name="username" autoComplete="email" type="text" className='inputEmail' required placeholder='' id='username' />
+                                    <input name="username" autoComplete="email" type="text" className='inputEmail' required placeholder='' id='username' />
                                 </div>
                             </div>
                         </div>
@@ -40,9 +90,9 @@ const LeftLogin = () => {
                 </fieldset>
                 <footer>
                     <button type="submit" className="btnLogin" disabled aria-disabled="true" name="login" >
-                    <div className='btnLoginText'>
-                        Next
-                    </div>    
+                        <div className='btnLoginText'>
+                            Next
+                        </div>
                     </button>
                     <span className='registerLogin'>
                         Don't Have an account?
