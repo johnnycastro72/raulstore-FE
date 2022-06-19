3b605aaaa597ef899354d6492c4d1d90a5247233
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-19 14:12:19 -0500

    ADD: Products
    Products feature implemented

--- src/app/features/product/ProductList.tsx
+++ src/app/features/product/ProductList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Alert, Button, Table } from 'react-bootstrap';
+import { Alert, Button, Form, Modal, Table } from 'react-bootstrap';
 import { useAppDispatch, useAppSelector } from '../../hooks'
 import { deleteProductAction, getAllProductsAction, getProductsById, Product } from './productAction';
 import ProductCreate from './ProductCreate';
@@ -13,6 +13,24 @@ const ProductList = () => {
     const productsErrors = useAppSelector(getProductsErrors)
 
     const [haveUnits, setHaveUnits] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+    const [actualProduct, setActualProduct] = useState<Product>({
+        name: "",
+        description: "",
+        units: 0,
+        price: 0,
+        minimumUnits: 0,
+        maximumUnits: 0,
+        productSupplierDTO: {
+            id: "",
+            taxPayerId: "",
+            supplierName: "",
+            supplierPhone: "",
+            supplierNotes: ""
+        }
+    })
+    const showOff = () => setShowModal(false)
+    const showOn = () => setShowModal(true)
 
     useEffect(() => {
         if (productsStatus === 'idle') {
@@ -51,7 +69,7 @@ const ProductList = () => {
                                 <td>{product.name}</td>
                                 <td>{product.description}</td>
                                 <td align='center'>{product.units}</td>
-                                <td align='center' width={15}><button className="tableBtn"><img style={{ width: "100%", height: "100%" }} src="https://firebasestorage.googleapis.com/v0/b/raul-s-hardware-store.appspot.com/o/assets%2Fview_icon.svg?alt=media&token=fcb028da-2de8-46ca-acfb-c1edc55d5872" alt="View Product" /></button></td>
+                                <td align='center' width={15}><button className="tableBtn" onClick={() => { showOn(); setActualProduct(product); }}><img style={{ width: "100%", height: "100%" }} src="https://firebasestorage.googleapis.com/v0/b/raul-s-hardware-store.appspot.com/o/assets%2Fview_icon.svg?alt=media&token=fcb028da-2de8-46ca-acfb-c1edc55d5872" alt="View Product" /></button></td>
                                 <td align='center' width={15}><button className="tableBtn" onClick={() => removeProduct(product.id as string)}><img style={{ width: "110%", height: "110%" }} src="https://firebasestorage.googleapis.com/v0/b/raul-s-hardware-store.appspot.com/o/assets%2FFalse.svg?alt=media&token=435aaf5a-9351-412f-84d4-cc5b5d68dec4" alt="Delete Product" /></button></td>
                             </tr>
                         )
@@ -68,6 +86,116 @@ const ProductList = () => {
                         <Button onClick={() => setHaveUnits(false)}>Close</Button>
                     </Alert>
                 }
+            </>
+            <>
+                <Modal
+                    show={showModal}
+                    size="lg"
+                    onHide={showOff}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Product Details</Modal.Title>
+                    </Modal.Header>
+                    <form>
+                        <Modal.Body>
+                            <div>
+                                <label>Product Name</label>
+                                <input
+                                    id="productName"
+                                    type="text"
+                                    name="productName"
+                                    className="titleInput"
+                                    placeholder="Product Name"
+                                    value={actualProduct.name}
+                                    style={{ marginLeft: '1rem', marginRight: '10%' }}
+                                    required
+                                    disabled
+                                />
+                                <label style={{ marginLeft: '10%' }}>Product Price</label>
+                                <input
+                                    id="price"
+                                    type="number"
+                                    name="price"
+                                    className="titleInput"
+                                    placeholder="Product Price"
+                                    value={actualProduct.price}
+                                    style={{ marginLeft: '1rem', marginRight: '0' }}
+                                    disabled
+                                    required
+                                />
+                            </div>
+                            <br />
+                            <label>Product Description</label>
+                            <Form.Control
+                                as="textarea"
+                                rows={2}
+                                id="description"
+                                name="description"
+                                placeholder="Product Description"
+                                value={actualProduct.description}
+                                style={{ marginLeft: '0', marginRight: '0' }}
+                                disabled
+                                required
+                            />
+                            <br />
+                            <div>
+                                <label>Minimum Units</label>
+                                <input
+                                    id="min"
+                                    type="number"
+                                    name="min"
+                                    className="titleInput"
+                                    placeholder="Minimum Units"
+                                    value={actualProduct.minimumUnits}
+                                    style={{ marginLeft: '1rem', marginRight: '8%' }}
+                                    disabled
+                                    required
+                                />
+                                <label style={{ marginLeft: '8.5%' }}>Maximum Units</label>
+                                <input
+                                    id="max"
+                                    type="number"
+                                    name="max"
+                                    className="titleInput"
+                                    placeholder="Maximum Units"
+                                    value={actualProduct.maximumUnits}
+                                    style={{ marginLeft: '1rem', marginRight: '0' }}
+                                    disabled
+                                    required
+                                />
+                            </div>
+                            <br />
+                            <div>
+                                <label>Product Supplier</label>
+                                <input
+                                    id="productSupplierDTO"
+                                    type="text"
+                                    name="productSupplierDTO"
+                                    className="titleInput"
+                                    placeholder="Produc Supplier"
+                                    value={actualProduct.productSupplierDTO.supplierName.toString()}
+                                    style={{ marginLeft: '1rem', marginRight: '0' }}
+                                    disabled
+                                    required
+                                />
+                                <label style={{ marginLeft: '16%' }}>Available Units</label>
+                                <input
+                                    id="units"
+                                    type="number"
+                                    name="units"
+                                    className="titleInput"
+                                    placeholder="Available Units"
+                                    value={actualProduct.units}
+                                    style={{ marginLeft: '1rem', marginRight: '0' }}
+                                    disabled
+                                    required
+                                />
+                            </div>
+                        </Modal.Body>
+                    </form>
+                </Modal>
             </>
 
         </>
